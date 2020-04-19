16a4951002fcfbeeaf50349840b027e1dbfa71b9
tempesttswinson <tempestt.swinson@gmail.com>
2020-04-19 02:18:24 -0400

    displays pic to represent weather added css

--- weatherApp.js
+++ weatherApp.js
@@ -2,9 +2,15 @@
 function convert(num){
 	return  Math.floor((num -273) * (9/5) +32);
 }
+// function setWeather(){
+// 	var weather =
+// }
 
 $('#submit').click(() =>{
 
+	let zip = $('#zipCode').val();
+
+	if (zip != "" ){
 	// var Moment = moment.tz().format('MMMM Do YYYY, h:mm a z');
 
 	var Moment = moment().format('MMMM Do YYYY, h:mm a');
@@ -12,20 +18,33 @@ $('#submit').click(() =>{
 	var display_time = $('#display_time');
 	display_time.text(Moment);
 
-	let zip = $('#zipCode').val();
 
 	$.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=678ba9df227448b5ef0a9f4ff586b675`, ((data)=>{
 
-		if(zip !== ''){
-
 			var display_weather = $('#display_weather');
+			var weather = data.weather[0].main;
 			console.log(data);
 			$('#city').text(data.name);
 			display_weather.text(convert(data.main.temp)+" F");
 			$('#clouds').text(data.weather[0].description);
-		}else{
-			display_weather.text("Enter Zip Code");
-		}
+
+			// determines which picture to display to represent the current weather condition
+			if (weather === 'Clear'){
+				var img = $('<img src= "https://cdn2.iconfinder.com/data/icons/weather-collection-1/512/sunny512x512-01-128.png"></img>');
+				$('.jumbotron').append(img);
+			}else if (weather === 'Clouds'){
+				var img = $('<img src= "https://cdn2.iconfinder.com/data/icons/weather-collection-1/512/rainnyday-128.png"></img>')
+				$('.jumbotron').append(img);
+			}else if (weather === 'Thunderstorm'){
+				var img = $('<img src= "images/thunderstorm-01-128.png"></img>')
+				$('.jumbotron').append(img);
+			}else if (weather === 'Rain' || weather === 'Drizzle'){
+				var img = $('<img src= "https://cdn2.iconfinder.com/data/icons/weather-collection-1/512/rainnyday-128.png"></img>')
+				$('.jumbotron').append(img);
+			}
 
 	}))
+	}else{
+		alert("Enter Zip Code!");
+	}
 });
