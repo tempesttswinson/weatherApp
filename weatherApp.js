5b088416985598be2358ec5a0f26769572c995eb
tempesttswinson <tempestt.swinson@gmail.com>
2020-04-19 22:41:49 -0400

    finalized weather app project

--- weatherApp.js
+++ weatherApp.js
@@ -1,58 +1,60 @@
-
+//converts the given temperature from Kelvin into Fahrenheit 
 function convert(num){
 	return  Math.floor((num -273) * (9/5) +32);
 }
-// function setWeather(){
-// 	var weather =
-// }
 
 $('#submit').click(() =>{
 
 	let zip = $('#zipCode').val();
 
+	//checks to make sure user has entered a value into the input field
 	if (zip != "" ){
 	
 
 		$.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=678ba9df227448b5ef0a9f4ff586b675`, ((data)=>{
 
-				// let Moment=
-	   //               moment().tz(data.timezone).format('dddd') + ', ' +
-	   //               moment().tz(data.timezone).format('hh:mm: z');
-	                             
-	            let timezone = data.timezone;
-	            let city = data.name;              
-				var Moment = moment.tz(`America/${city}`).format('MMMM Do YYYY, h:mm a z');
-
-				//var Moment = moment().format('MMMM Do YYYY, h:mm a');
-
-				var display_time = $('#display_time');
-				display_time.text(Moment);
-
-
-				var display_weather = $('#display_weather');
-				var weather = data.weather[0].main;
+	            //displays the returned object to the console 
 				console.log(data);
-				$('#city').text(city);
-				display_weather.text(convert(data.main.temp)+" F");
-				$('#clouds').text(data.weather[0].description);
 
-				// determines which picture to display to represent the current weather condition
+	            //sets all my variables
+	            let city = data.name; 
+	            let lon = data.coord.lon;           
+	            let lat = data.coord.lat; 
+	            let weather = data.weather[0].main;
+	           	let icon = data.weather[0].icon;
+	            //creates an img tag and sets its src to the url of the returned icon 
+	           	let img= $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + icon + '@2x.png'); 
+
+
+				//grabs the elements from HTML and sets their text to the information produced from API object so that it can be displayed on the browser
+				$('#city').text(data.name);
+				//calls the function convert to set temp in Fahrenheit 
+				$('#display_weather').text(convert(data.main.temp)+" F");
+				$('#clouds').text(data.weather[0].description);
+				$('#weather_pic').html(img);
+
+				// determines which picture to display to the background according to the  current weather condition
 				if (weather === 'Clear'){
-					var img = $('<img src= "https://cdn2.iconfinder.com/data/icons/weather-collection-1/512/sunny512x512-01-128.png"></img>');
-					$('.jumbotron').append(img);
+					$('body').addClass('clear');
 				}else if (weather === 'Clouds'){
-					var img = $('<img src= "https://cdn2.iconfinder.com/data/icons/weather-collection-1/512/rainnyday-128.png"></img>')
-					$('.jumbotron').append(img);
+					$('body').addClass('clouds');
 				}else if (weather === 'Thunderstorm'){
-					var img = $('<img src= "images/thunderstorm-01-128.png"></img>')
-					$('.jumbotron').append(img);
+					$('body').addClass('thunderstorm');
 				}else if (weather === 'Rain' || weather === 'Drizzle'){
-					var img = $('<img src= "https://cdn2.iconfinder.com/data/icons/weather-collection-1/512/rainnyday-128.png"></img>')
-					$('.jumbotron').append(img);
+					$('body').addClass('rain');
 				}
 
+				//uses lon and lat of the zipcode that was entered to go to the location of that lat and lon api
+				$.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=678ba9df227448b5ef0a9f4ff586b675&units=imperial`, ((data2)=>{
+
+						//grabs the timezone of the lon and lat and formats it using the moment library then displays on the browser
+						let datetime= moment().tz(data2.timezone).format('dddd MMMM Do YYYY, h:mm a z');
+			            $('#display_time').text(datetime);
+				}))
 		}))
-		}else{
-			alert("Enter Zip Code!");
-		}
+	}else{
+		alert("Enter Zip Code!");
+	}
+
+		
 });
